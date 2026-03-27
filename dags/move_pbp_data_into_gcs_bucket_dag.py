237dbe6a65b8d11f1b1166764fde48e4ccd973bc
Matt <mcoulomb7@gmail.com>
2026-03-26 21:47:16 -0500

    updated move_pbp_data_into_gcs_bucket_dag and created create_bigquery_external_table_dag.

--- dags/move_pbp_data_into_gcs_bucket_dag.py
+++ dags/move_pbp_data_into_gcs_bucket_dag.py
@@ -4,12 +4,14 @@ from airflow.providers.standard.operators.bash import BashOperator
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 import tarfile
 from airflow.sdk import task
+from airflow.models import Param
 
 @task(task_id="extract_tar")
 def extract_tar(**context):
+    dataset_name = f"{context["params"]["dataset"]}_{context["params"]["year"]}"
     # Open and extract the .tar.xz file
-    with tarfile.open('dags/nbastats_2016.tar.xz', "r:xz") as tar:
-        tar.extract('nbastats_2016.csv',dag.folder)  # Extract to a specified directory   
+    with tarfile.open(f'dags/{dataset_name}.tar.xz', "r:xz") as tar:
+        tar.extract(f'{dataset_name}.csv',dag.folder)  # Extract to a specified directory   
 
 # Default arguments for the DAG
 default_args = {
@@ -27,21 +29,28 @@ dag = DAG(
     schedule='@daily',
     catchup=False,
     tags=['example', 'hello-world'],
+    params={
+        "year": Param("2025", type="string"),
+        "dataset": Param("cdnnba", type="string")
+    }
 )
 
 # Define tasks
-download_task = BashOperator(task_id='download_task',cwd=dag.folder, bash_command='wget https://github.com/shufinskiy/nba_data/raw/refs/heads/main/datasets/nbastats_2016.tar.xz',dag=dag)
+download_task = BashOperator(task_id='download_task',cwd=dag.folder, bash_command=f'wget https://github.com/shufinskiy/nba_data/raw/refs/heads/main/datasets/{dag.params["dataset"]}_{dag.params["year"]}.tar.xz',dag=dag)
 upload_file = LocalFilesystemToGCSOperator(
     task_id="upload_file",
-    src=f"{dag.folder}/nbastats_2016.csv",
-    dst='nbastats_2016.csv',
+    src=f"{dag.folder}/{dag.params["dataset"]}_{dag.params["year"]}.csv",
+    dst=f'{dag.params["dataset"]}_{dag.params["year"]}.csv',
     bucket='mcoulomb-nba-pbp-data',
     gcp_conn_id='gcp',
     dag=dag
 )
 
+delete_tar_files = BashOperator(task_id='delete_tar_files_task',cwd=dag.folder, bash_command='find . -name "*.tar.xz" -type f -delete',dag=dag, trigger_rule="all_done")
+delete_csv_files = BashOperator(task_id='delete_csv_files_task',cwd=dag.folder, bash_command='find . -name "*.csv" -type f -delete',dag=dag, trigger_rule="all_done")
+
 # Set task dependencies
-download_task >> extract_tar() >> upload_file   
+download_task >> extract_tar() >> upload_file >> delete_tar_files >> delete_csv_files   
 
 #if __name__ == "__main__":
  #   dag.test()
