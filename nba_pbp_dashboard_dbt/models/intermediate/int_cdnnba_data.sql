70a98151ead00a00696b6e97fa87b87d66565f7b
Matt <mcoulomb7@gmail.com>
2026-04-02 13:48:15 -0500

    Cleaned up existing models, added column descriptions, new derived columns in int_cdnnba_data model, and more column data testing.

--- nba_pbp_dashboard_dbt/models/intermediate/int_cdnnba_data.sql
+++ nba_pbp_dashboard_dbt/models/intermediate/int_cdnnba_data.sql
@@ -36,9 +36,11 @@ with int_cdnnba_data as (
         period,
         periodType,
         clock,
-        actual_time,
+        CAST(substr(clock, 3, 2) as integer) as minutes_remaining,
+        CAST(substr(clock, 6, 5) as numeric) as seconds_remaining,
         home_score,
         away_score,
+        abs(home_score - away_score) as score_diff,
 
         -- actions
         action_type,
@@ -48,13 +50,28 @@ with int_cdnnba_data as (
         area_detail,
         shot_distance,
         shot_result,
+        x_location,
+        y_location,
         description,
         descriptor,
         is_field_goal
 
     from {{ ref('stg_cdnnba_data') }}
+),
+
+int_pbp_data as (
+    select 
+            CASE
+            WHEN (score_diff >= 25 AND period >= 4 AND minutes_remaining BETWEEN 9 and 12 AND seconds_remaining BETWEEN 0 AND 59.99) THEN true
+            WHEN (score_diff >= 20 AND period >= 4 AND minutes_remaining BETWEEN 6 and 9 AND seconds_remaining BETWEEN 0 AND 59.99) THEN true
+            WHEN (score_diff >= 15 AND period >= 4 AND minutes_remaining BETWEEN 1 and 6 AND seconds_remaining BETWEEN 0 AND 59.99) THEN true
+            WHEN (score_diff >= 9  AND period >= 4 AND minutes_remaining = 0 AND seconds_remaining BETWEEN 0 AND 59.99) THEN true
+            ELSE false
+            END as is_garbage_time,
+            *
+    from int_cdnnba_data
 )
 
-select * from int_cdnnba_data
+select * from int_pbp_data
 
 
--- nba_pbp_dashboard_dbt/models/marts/dim_player_data.sql
+++ nba_pbp_dashboard_dbt/models/marts/dim_player_data.sql
@@ -1,6 +1,6 @@
 
 /*
-    Staging table to materialize external pbp table.
+    Dimension table for players. The MAX aggregation is used on the player_name field to ensure that we get a single name for each player_id and team_tricode combination.
 */
 
 {{ config(
@@ -11,10 +11,11 @@
 with int_player_data as (
     select distinct
         player_id,
-        player_name,
+        MAX(player_name) as player_name,
         team_tricode
      from {{ ref('int_cdnnba_data') }}
      where player_id != 0 and player_name is not null
+     GROUP BY player_id, team_tricode
 )
 
 select * from int_player_data
